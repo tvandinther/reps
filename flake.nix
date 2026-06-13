caae30bc6eb725f842911fddc5f5f65ebc9c62f2
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2026-06-13 17:51:28 +0200

    configure env

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
   outputs = { self, android-nixpkgs }: {
     packages.x86_64-linux.android-sdk = android-nixpkgs.sdk.x86_64-linux (sdkPkgs: with sdkPkgs; [
       cmdline-tools-latest
-      build-tools-35-0-0
+      build-tools-34-0-0
       platform-tools
       platforms-android-35
       emulator
